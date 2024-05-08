49477484d547b6ee8f586fa92e1be6d3394084ee
AdrianGutierrez019 <adrian.guitierrez@automation-power.com>
2024-05-08 12:01:02 -0500

    Mi mensaje

--- funciones/Privacy.js
+++ funciones/Privacy.js
@@ -2,21 +2,10 @@ import {getPrivacy} from '@/funciones/getPrivacy'
 import PriStyle from '@/styles/Privacy.module.css'
 
 const PPrivacy = async () => {
-    const Privacy =await getPrivacy('Privacy');
+    //const Privacy =await getPrivacy('Privacy');
     return (
         <div >
-            <div className={PriStyle.Cont2}>
-                <h2 className={PriStyle.title}>Privacy Policy</h2>
-                    <list className={PriStyle.flexi}>
-                        <a className={PriStyle.title01} href="Home">Home</a>
-                        <li className={PriStyle.Desc01}>Privacy</li>
-                    </list>
-            </div>
-            <div className={PriStyle.Cont3}>
-                    <h2 className={PriStyle.title1}>{Privacy.Title}</h2>
-                    <p className={PriStyle.desc}>{Privacy.Descripcion}</p>
-                <div className={PriStyle.Cont4} dangerouslySetInnerHTML={{ __html: Privacy.Info }}></div>
-            </div>
+
         </div>
     );
 };
